655a994a69a76ef72fba31272a93cc8cbf2ef2ce
Chala Birmechu <chalabirmechu@gmail.com>
2025-10-27 18:32:20 +0300

    Update loading spinner timing in script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener('load', () => {
   loading.style.opacity = '0';
   setTimeout(() => {
     loading.style.display = 'none';
-  }, 15000);
+  }, 1000);
 });
 
 // Mobile Navigation
